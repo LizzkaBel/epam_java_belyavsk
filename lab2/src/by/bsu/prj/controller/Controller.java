e2a1dbf8835de61ba53299e5065769130181ac0d
LizzkaBel <mmf.belyavsk@gmail.com>
2020-03-22 22:03:25 +0300

    I want to sleeeeep

--- lab2/src/by/bsu/prj/controller/Controller.java
+++ lab2/src/by/bsu/prj/controller/Controller.java
@@ -12,20 +12,34 @@ package by.bsu.prj.controller;
 
 @WebServlet(urlPatterns = "/controller")
 public class Controller extends HttpServlet {
-    static Logger log = LogManager.getLogger(Controller.class.getName());
 
+
+    Logger logger = LogManager.getLogger();
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String firstNumberStr = request.getParameter("num1");
+        String secondNumberStr = request.getParameter("num2");
+        logger.info("Got 2 strings");
+        int firstNumber = Integer.parseInt(firstNumberStr);
+        int secondNumber = Integer.parseInt(secondNumberStr);
+        logger.info("Parsed strings to numbers");
+        int sum = firstNumber + secondNumber;
+        request.setAttribute("num1", firstNumber);
+        request.setAttribute("num2", secondNumber);
+        request.setAttribute("sum", sum);
+        request.getRequestDispatcher("pages/main.jsp").forward(request, response);
+        logger.info("Sent request");
 
-        String numberStr1 = request.getParameter("firstNumber");
-        String numberStr2 = request.getParameter("secondNumber");
-        int num1 = Integer.parseInt(numberStr1);
-        int num2 = Integer.parseInt(numberStr2);
-        int result = num1 * num2;
 
-
-
-        request.setAttribute("res",result);
+        request.setAttribute("num1", firstNumber);
+        request.setAttribute("num2", secondNumber);
+        request.setAttribute("sum", sum);
         request.getRequestDispatcher("pages/main.jsp").forward(request, response);
     }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+
 }
